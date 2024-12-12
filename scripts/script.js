ed1bbda5f02594bf64eb5394d3ea325e1c2c8c10
JamieBuffing <78273478+JamieBuffing@users.noreply.github.com>
2024-12-12 23:09:02 +0100

    her

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,8 @@
 let favorieten = 0
 
+let r = document.querySelector(':root');
+let rs = getComputedStyle(r);
+
 function hamenuOpen() {
     document.documentElement.scrollTop = 0;
     document.querySelector("nav").style.marginLeft = '0vw';
@@ -33,4 +36,32 @@ function heart() {
     favorieten ++;
     console.log(favorieten);
     document.getElementById("number").innerText = favorieten;
+}
+
+
+function theme() {
+    let r = document.querySelector(':root');
+    if(rs.getPropertyValue('--kaart2') == "#203a2b") {
+        /*Light mode*/
+        r.style.setProperty('--white-text', '#FFFFFF');
+        r.style.setProperty('--dark-text', '#000000');
+        r.style.setProperty('--yellow-text', '#d5ea42');
+        r.style.setProperty('--background', '#FFFFFF');
+        r.style.setProperty('--background2', '#000000');
+        r.style.setProperty('--kaarten', '#262728;');
+        r.style.setProperty('--kaart2', '#355c46');
+        r.style.setProperty('--info', '#f9f9f9');
+        document.getElementById("modeimg").style.rotate = "180deg"
+    } else {
+        /*Dark mode*/
+        r.style.setProperty('--white-text', '#000000');
+        r.style.setProperty('--dark-text', '#FFFFFF');
+        r.style.setProperty('--yellow-text', '#d5ea42');
+        r.style.setProperty('--background', '#7a7a7a');
+        r.style.setProperty('--background2', '#d4d4d4');
+        r.style.setProperty('--kaarten', '#4f5153;');
+        r.style.setProperty('--kaart2', '#203a2b');
+        r.style.setProperty('--info', '#afaeae');
+        document.getElementById("modeimg").style.rotate = "0deg"
+    }
 }
